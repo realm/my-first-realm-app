9e48f3a57b1dd8c2289af1e90e993cd9a13c4f47
Emanuele Zattin <ez@realm.io>
2018-01-22 13:11:01 +0100

    Add a README file and Constants file

---
+++ android/app/src/main/java/io/realm/todo/Constants.java
@@ -0,0 +1,7 @@
+package io.realm.todo;
+
+final class Constants {
+    private static final String INSTANCE_ADDRESS = "MY_INSTANCE_ADDRESS";
+    static final String AUTH_URL = "https://" + INSTANCE_ADDRESS;
+    static final String REALM_URL = "realms://" + INSTANCE_ADDRESS;
+}
