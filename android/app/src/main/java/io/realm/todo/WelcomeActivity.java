84e9ea3119f0c4fdd1eafdc2aeded13b4abaae3a
Nabil Hachicha <nabil.hachicha@gmail.com>
2018-01-22 17:35:08 +0000

    Using realm-android-adapter library & various cleanup (#1)
    * Using realm-android-adapter library to handle RecyclerView
    Various cleanup
    Add licence header
    
    * PR feedback
    
    * Removed dead code

--- android/app/src/main/java/io/realm/todo/WelcomeActivity.java
+++ android/app/src/main/java/io/realm/todo/WelcomeActivity.java
@@ -4,16 +4,11 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-
-import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -25,9 +20,6 @@ import static io.realm.todo.Constants.AUTH_URL;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    private LoginTask mAuthTask = null;
-
-    // UI references.
     private EditText mNicknameTextView;
     private View mProgressView;
     private View mLoginFormView;
@@ -44,28 +36,34 @@ public class WelcomeActivity extends AppCompatActivity {
         // Set up the login form.
         mNicknameTextView = findViewById(R.id.nickname);
         Button loginButton = findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
+        loginButton.setOnClickListener(view -> attemptLogin());
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
 
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
         // Reset errors.
         mNicknameTextView.setError(null);
         // Store values at the time of the login attempt.
         String nickname = mNicknameTextView.getText().toString();
         showProgress(true);
-        mAuthTask = new LoginTask(nickname);
-        mAuthTask.execute((Void) null);
+
+        SyncCredentials credentials = SyncCredentials.nickname(nickname, true);
+        SyncUser.loginAsync(credentials, AUTH_URL, new SyncUser.Callback<SyncUser>() {
+            @Override
+            public void onSuccess(SyncUser user) {
+                showProgress(false);
+                goToItemsActivity();
+            }
+
+            @Override
+            public void onError(ObjectServerError error) {
+                showProgress(false);
+                mNicknameTextView.setError("Uh oh something went wrong! (check your logcat please)");
+                mNicknameTextView.requestFocus();
+                Log.e("Login error", error.toString());
+            }
+        });
     }
 
     /**
@@ -95,48 +93,6 @@ public class WelcomeActivity extends AppCompatActivity {
     private void goToItemsActivity(){
         Intent intent = new Intent(WelcomeActivity.this, ItemsActivity.class);
         startActivity(intent);
-    }
-
-
-    public class LoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mNickname;
-
-        LoginTask(String nickname) {
-            mNickname = nickname;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-            SyncCredentials creds = SyncCredentials.nickname(mNickname, true);
-            try {
-                SyncUser user = SyncUser.login(creds, AUTH_URL);
-            } catch (ObjectServerError e) {
-                Log.d("Login", e.getErrorMessage());
-                return false;
-            }
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                goToItemsActivity();
-            } else {
-                mNicknameTextView.setError("Uh oh something went wrong!");
-                mNicknameTextView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
     }
 }
 
