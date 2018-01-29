02ff905574d1dd0e5ae3da6d1f358954ca0966be
David Spector <spector@zeitgeist.com>
2018-01-29 11:35:29 -0800

    removed custom cloud endpoints again

--- ios/PartialSync/ToDo/Constants.swift
+++ ios/PartialSync/ToDo/Constants.swift
@@ -9,12 +9,10 @@ struct Constants {
     // **** Replace `https://MY_INSTANCE_ADDRESS ` with the address of your
     // **** your ROS server, e.g.: "http://127.0.0.1:9080"
 
-    // static let AUTH_URL = URL(string: "https://MY_INSTANCE_ADDRESS")!
-    static let AUTH_URL = URL(string: "https://todo.us1.cloud.realm.io")!
+    static let AUTH_URL = URL(string: "https://MY_INSTANCE_ADDRESS")!
 
     static var REALM_URL: URL {
-        // return URL(string: "realms://MY_INSTANCE_ADDRESS/ToDo")!
-        return URL(string: "realms://todo.us1.cloud.realm.io/ToDo")!
+        return URL(string: "realms://MY_INSTANCE_ADDRESS/ToDo")!
     }
 }
 
