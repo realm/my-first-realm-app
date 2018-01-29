8c0f3dda6f18a184f14ce8f225b52e7876d37359
David Spector <spector@zeitgeist.com>
2018-01-28 23:11:36 -0800

    checking test instance addresses back in to help debug some cloud issues

--- ios/PartialSync/ToDo/Constants.swift
+++ ios/PartialSync/ToDo/Constants.swift
@@ -9,10 +9,13 @@ struct Constants {
     // **** Replace `https://MY_INSTANCE_ADDRESS ` with the address of your
     // **** your ROS server, e.g.: "http://127.0.0.1:9080"
 
-    static let AUTH_URL = URL(string: "https://MY_INSTANCE_ADDRESS")!
+    // static let AUTH_URL = URL(string: "https://MY_INSTANCE_ADDRESS")!
+    static let AUTH_URL = URL(string: "https://todo.us1.cloud.realm.io")!
 
     static var REALM_URL: URL {
-        return URL(string: "realms://MY_INSTANCE_ADDRESS")!
+        // return URL(string: "realms://MY_INSTANCE_ADDRESS/ToDo")!
+        return URL(string: "realms://todo.us1.cloud.realm.ioTo/ToDo")!
     }
 }
+
 
