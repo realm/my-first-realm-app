9720f151721f3bd3d050e620c9507441c8472824
David Spector <spector@zeitgeist.com>
2018-01-31 11:06:08 -0800

    update constants to match narrative in tutorial

--- ios/PartialSync/ToDo/Constants.swift
+++ ios/PartialSync/ToDo/Constants.swift
@@ -2,18 +2,19 @@
 import Foundation
 struct Constants {
     // **** Realm Cloud Users:
-    // **** Replace the "MY_INSTANCE_ADDRESS" here with the hostname of your
-    // **** Realm Cloud instance or the URL e.g., "mycoolapp.us1.cloud.realm.io"
+    // **** Replace MY_INSTANCE_ADDRESS with the hostname of your cloud instance
+    // **** e.g., "mycoolapp.us1.cloud.realm.io"
+    // ****
     // ****
     // **** ROS On-Premises Users
-    // **** Replace `https://MY_INSTANCE_ADDRESS ` with the address of your
-    // **** your ROS server, e.g.: "http://127.0.0.1:9080"
-
-    static let AUTH_URL = URL(string: "https://MY_INSTANCE_ADDRESS")!
-
-    static var REALM_URL: URL {
-        return URL(string: "realms://MY_INSTANCE_ADDRESS/ToDo")!
-    }
+    // **** Replace the AUTH_URL and REALM_URL strings with the fully qualified versions of
+    // **** address of your ROS server, e.g.: "http://127.0.0.1:9080" and "realm://127.0.0.1:9080"
+    
+    static let MY_INSTANCE_ADDRESS = "MY_INSTANCE_ADDRESS" // <- update this
+    
+    static let AUTH_URL  = URL(string: "https://\(MY_INSTANCE_ADDRESS)")!
+    static let REALM_URL = URL(string: "realms://\(MY_INSTANCE_ADDRESS)/ToDo")!
 }
+
 
 
--- ios/SyncIntro/ToDo/Constants.swift
+++ ios/SyncIntro/ToDo/Constants.swift
@@ -1,17 +1,18 @@
 
 import Foundation
 struct Constants {
+    // **** Realm Cloud Users:
+    // **** Replace MY_INSTANCE_ADDRESS with the hostname of your cloud instance
+    // **** e.g., "mycoolapp.us1.cloud.realm.io"
     // ****
-    // **** Replace the "MY_INSTANCE_ADDRESS" here with the hostname of your
-    // **** Realm Cloud instance or the URL of your on-premises Realm Object Server
     // ****
-
-    //static let AUTH_URL = URL(string: "https://MY_INSTANCE_ADDRESS")!
-    static let AUTH_URL = URL(string: "https://todo.us1.cloud.realm.io")!
-
-    static var REALM_URL: URL {
-        let realmsUrlString = Constants.AUTH_URL.absoluteString.replacingOccurrences(of: "https://", with: "realms://")
-        return URL(string: "\(realmsUrlString)/ToDo")!
-    }
+    // **** ROS On-Premises Users
+    // **** Replace the AUTH_URL and REALM_URL strings with the fully qualified versions of
+    // **** address of your ROS server, e.g.: "http://127.0.0.1:9080" and "realm://127.0.0.1:9080"
     
+    static let MY_INSTANCE_ADDRESS = "MY_INSTANCE_ADDRESS" // <- update this
+    
+    static let AUTH_URL  = URL(string: "https://\(MY_INSTANCE_ADDRESS)")!
+    static let REALM_URL = URL(string: "realms://\(MY_INSTANCE_ADDRESS)/ToDo")!
 }
+
