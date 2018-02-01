6dfed1000748d7c36a424f7732865eafafcbb244
David Spector <spector@zeitgeist.com>
2018-02-01 14:08:32 -0800

    reformatting Contants comments.

--- ios/PartialSync/ToDo/Constants.swift
+++ ios/PartialSync/ToDo/Constants.swift
@@ -7,14 +7,13 @@ struct Constants {
     // ****
     // ****
     // **** ROS On-Premises Users
-    // **** Replace the AUTH_URL and REALM_URL strings with the fully qualified versions of
-    // **** address of your ROS server, e.g.: "http://127.0.0.1:9080" and "realm://127.0.0.1:9080"
+    // **** Replace the AUTH_URL and REALM_URL strings with the
+    // **** fully qualified versions of address of your ROS server, e.g.:
+    // **** "http://127.0.0.1:9080" and "realm://127.0.0.1:9080"
     
     static let MY_INSTANCE_ADDRESS = "MY_INSTANCE_ADDRESS" // <- update this
-    
+
     static let AUTH_URL  = URL(string: "https://\(MY_INSTANCE_ADDRESS)")!
     static let REALM_URL = URL(string: "realms://\(MY_INSTANCE_ADDRESS)/ToDo")!
 }
-
-
 
