940fa6676c64d34875ca3e960ed67007ceb1b512
Mark Rowe <mrowe@bdash.net.nz>
2018-03-05 19:36:58 -0800

    Use a different Realm URL than for earlier tutorials, to avoid problems due to the different schemas.

--- ios/ObjectPermissions/ToDo/Constants.swift
+++ ios/ObjectPermissions/ToDo/Constants.swift
@@ -1,5 +1,5 @@
-
 import Foundation
+
 struct Constants {
     // **** Realm Cloud Users:
     // **** Replace MY_INSTANCE_ADDRESS with the hostname of your cloud instance
@@ -14,6 +14,5 @@ struct Constants {
     static let MY_INSTANCE_ADDRESS = "MY_INSTANCE_ADDRESS" // <- update this
 
     static let AUTH_URL  = URL(string: "https://\(MY_INSTANCE_ADDRESS)")!
-    static let REALM_URL = URL(string: "realms://\(MY_INSTANCE_ADDRESS)/ToDo")!
+    static let REALM_URL = URL(string: "realms://\(MY_INSTANCE_ADDRESS)/ToDo-permissions")!
 }
-
