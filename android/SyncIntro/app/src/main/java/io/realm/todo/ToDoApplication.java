2b786a50ac88ad12151121e33a38a8782e4245e7
Jørgen Edelbo <je@realm.io>
2018-03-15 13:09:59 +0100

    Strip off Realm stuff from SyncIntro
    Remove all Realm related code. This can then be the baseline for
    tutorial on how to gradually adding Realm functionality.

--- android/SyncIntro/app/src/main/java/io/realm/todo/ToDoApplication.java
+++ android/SyncIntro/app/src/main/java/io/realm/todo/ToDoApplication.java
@@ -19,12 +19,10 @@ package io.realm.todo;
 
 import android.app.Application;
 
-import io.realm.Realm;
 
 public class ToDoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Realm.init(this);
     }
 }
--- android/SyncIntro/app/src/main/java/io/realm/todo/model/Item.java
+++ android/SyncIntro/app/src/main/java/io/realm/todo/model/Item.java
@@ -20,19 +20,10 @@ package io.realm.todo.model;
 import java.util.Date;
 import java.util.UUID;
 
-import io.realm.RealmObject;
-import io.realm.annotations.PrimaryKey;
-import io.realm.annotations.Required;
-
-public class Item extends RealmObject {
-    @PrimaryKey
-    @Required
+public class Item  {
     private String itemId;
-    @Required
     private String body;
-    @Required
     private Boolean isDone;
-    @Required
     private Date timestamp;
 
     public String getItemId() {
