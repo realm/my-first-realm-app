15d38d9289d68bac9d7ffe4a9b01b2455e766117
Nabil Hachicha <nabil.hachicha@gmail.com>
2018-04-10 21:10:25 +0100

    Logging in and out
    First initialize Realm. Then log into the cloud using the instance URL.

--- android/SyncIntro/app/src/main/java/io/realm/todo/ToDoApplication.java
+++ android/SyncIntro/app/src/main/java/io/realm/todo/ToDoApplication.java
@@ -19,10 +19,13 @@ package io.realm.todo;
 
 import android.app.Application;
 
+import io.realm.Realm;
+
 
 public class ToDoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Realm.init(this);
     }
 }
