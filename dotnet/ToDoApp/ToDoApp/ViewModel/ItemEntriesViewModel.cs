2f71995042be5bf6f8b9d1a9e58f0ebcdbde338d
Nikola Irinchev <irinchev@me.com>
2019-06-18 18:22:46 -0400

    Start

--- dotnet/ToDoApp/ToDoApp/ViewModel/ItemEntriesViewModel.cs
+++ dotnet/ToDoApp/ToDoApp/ViewModel/ItemEntriesViewModel.cs
@@ -15,10 +15,6 @@ namespace ToDoApp
 {
     public class ItemEntriesViewModel : INotifyPropertyChanged
     {
-        public IEnumerable<Item> Entries { get; private set; }
-
-        private Realm _realm;
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ICommand DeleteEntryCommand { get; private set; }
@@ -27,44 +23,11 @@ namespace ToDoApp
 
         public ItemEntriesViewModel()
         {
-            DeleteEntryCommand = new Command<Item>(DeleteEntry);
-            AddEntryCommand = new Command(() => AddEntry());
             Initialize();
         }
 
         private async Task Initialize()
         {
-            _realm = await OpenRealm();
-            Entries = _realm.All<Item>().OrderBy(i => i.Timestamp);
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Entries)));
-        }
-
-        private async Task AddEntry()
-        {
-            var result = await UserDialogs.Instance.PromptAsync(new PromptConfig
-            {
-                Title = "New entry",
-                Message = "Specify the item text",
-            });
-
-            if (result.Ok)
-            {
-                _realm.Write(() =>
-                {
-                    _realm.Add(new Item
-                    {
-                        Timestamp = DateTimeOffset.Now,
-                        Body = result.Value,
-                    });
-                });
-            }
-        }
-
-        private void DeleteEntry(Item entry) => _realm.Write(() => _realm.Remove(entry));
-
-        private async Task<Realm> OpenRealm()
-        {
-            return Realm.GetInstance();
         }
     }
 }
