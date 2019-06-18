c8fc30a017be49bbbac7b2582bda12d66e35fdb1
Nikola Irinchev <irinchev@me.com>
2019-06-18 17:45:14 -0400

    Final state

--- dotnet/ToDoApp/ToDoApp/Model/Item.cs
+++ dotnet/ToDoApp/ToDoApp/Model/Item.cs
@@ -5,17 +5,8 @@ namespace ToDoApp
 {
     public class Item : RealmObject
     {
-        [PrimaryKey]
-        [MapTo("itemId")]
-        public string ItemId { get; set; } = Guid.NewGuid().ToString();
-
-        [MapTo("body")]
         public string Body { get; set; }
 
-        [MapTo("isDone")]
-        public bool IsDone { get; set; }
-
-        [MapTo("timestamp")]
         public DateTimeOffset Timestamp { get; set; }
     }
 }
--- dotnet/ToDoApp/ToDoApp/ViewModel/ItemEntriesViewModel.cs
+++ dotnet/ToDoApp/ToDoApp/ViewModel/ItemEntriesViewModel.cs
@@ -15,6 +15,9 @@ namespace ToDoApp
 {
     public class ItemEntriesViewModel : INotifyPropertyChanged
     {
+        public static string AuthUrl = "https://mdbw19.us1.cloud.realm.io";
+        public static string RealmPath = "/~/ToDo";
+
         public IEnumerable<Item> Entries { get; private set; }
 
         private Realm _realm;
@@ -28,8 +31,8 @@ namespace ToDoApp
         public ItemEntriesViewModel()
         {
             DeleteEntryCommand = new Command<Item>(DeleteEntry);
-            AddEntryCommand = new Command(() => AddEntry().IgnoreResult());
-            Initialize().IgnoreResult();
+            AddEntryCommand = new Command(() => AddEntry());
+            Initialize();
         }
 
         private async Task Initialize()
@@ -37,7 +40,6 @@ namespace ToDoApp
             _realm = await OpenRealm();
             Entries = _realm.All<Item>().OrderBy(i => i.Timestamp);
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Entries)));
-
         }
 
         private async Task AddEntry()
@@ -66,56 +68,14 @@ namespace ToDoApp
         private async Task<Realm> OpenRealm()
         {
             var user = User.Current;
-            if (user != null)
+            if (user == null)
             {
-                var configuration = new FullSyncConfiguration(new Uri(Constants.RealmPath, UriKind.Relative), user);
-
-                // User has already logged in, so we can just load the existing data in the Realm.
-                return Realm.GetInstance(configuration);
+                var credentials = Credentials.Nickname("Peter");
+                user = await User.LoginAsync(credentials, new Uri(AuthUrl));
             }
 
-            // When that is called in the page constructor, we need to allow the UI operation
-            // to complete before we can display a dialog prompt.
-            await Task.Yield();
-
-            var response = await UserDialogs.Instance.PromptAsync(new PromptConfig
-            {
-                Title = "Login",
-                Message = "Please enter your nickname",
-                OkText = "Login",
-                IsCancellable = false,
-            });
-            var credentials = Credentials.Nickname(response.Value, isAdmin: true);
-
-            try
-            {
-                UserDialogs.Instance.ShowLoading("Logging in...");
-
-                user = await User.LoginAsync(credentials, new Uri(Constants.AuthUrl));
-
-                UserDialogs.Instance.ShowLoading("Loading data");
-
-                var configuration = new FullSyncConfiguration(new Uri(Constants.RealmPath, UriKind.Relative), user);
-
-                // First time the user logs in, let's use GetInstanceAsync so we fully download the Realm
-                // before letting them interract with the UI.
-                var realm = await Realm.GetInstanceAsync(configuration);
-
-                UserDialogs.Instance.HideLoading();
-
-                return realm;
-            }
-            catch (Exception ex)
-            {
-                await UserDialogs.Instance.AlertAsync(new AlertConfig
-                {
-                    Title = "An error has occurred",
-                    Message = $"An error occurred while trying to open the Realm: {ex.Message}"
-                });
-
-                // Try again
-                return await OpenRealm();
-            }
+            var configuration = new FullSyncConfiguration(new Uri(RealmPath, UriKind.Relative), user);
+            return Realm.GetInstance(configuration);
         }
     }
 }
