3fc697f6e563993f94b9d8e475affde83cf05326
Kræn Hansen <kraen.hansen@realm.io>
2020-02-20 11:41:26 +0100

    Implementing the app

---
+++ react-native/full-sync/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import {Button} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {LoginScreen} from './LoginScreen';
+import {ItemsScreen} from './ItemsScreen';
+
+const Stack = createStackNavigator();
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={Realm.Sync.User.current ? 'Items' : 'Login'}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Items"
+          component={ItemsScreen}
+          options={({navigation}) => ({
+            headerTitle: 'Things ToDo!',
+            headerRight: () => (
+              <Button
+                title="Logout"
+                onPress={() => {
+                  if (Realm.Sync.User.current) {
+                    Realm.Sync.User.current.logout();
+                  }
+                  navigation.replace('Login');
+                }}
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
